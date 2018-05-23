1e9c71682f5596aa5ef855a4b09105e7d54abec8
Maksym Mazurkevych <maksym_mazurkevych@epam.com>
2018-05-23 11:06:59 +0300

    Initialized property

---
+++ README.md
@@ -0,0 +1,3 @@
+Spring Demo
+javabrains.org
+
--- src/main/java/org/javabrains/Triangle.java
+++ src/main/java/org/javabrains/Triangle.java
@@ -2,7 +2,18 @@ package org.javabrains;
 
 public class Triangle {
 
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    private String type;
+
     public void draw(){
+        System.out.println("The type : " + getType());
         System.out.println("Triangle has been draw");
     }
 }
