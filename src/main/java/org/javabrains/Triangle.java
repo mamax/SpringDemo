7661ddaed2b83c83cb75b1c1b59b33a49a508e69
Maksym Mazurkevych <mmmazurkevych@gmail.com>
2018-05-23 12:24:29 +0300

    Injected objects

--- src/main/java/org/javabrains/Triangle.java
+++ src/main/java/org/javabrains/Triangle.java
@@ -1,39 +1,18 @@
 package org.javabrains;
 
+import lombok.Data;
+
+@Data
 public class Triangle {
 
-    public String getType() {
-        return type;
-    }
-//
-//    public void setType(String type) {
-//        this.type = type;
-//    }
-
-    public Triangle(String type, int height) {
-        this.type = type;
-        this.height = height;
-    }
-
-    public Triangle(String type) {
-        this.type = type;
-    }
-
-    public Triangle(int height) {
-        this.height = height;
-    }
-
-    private String type;
-
-    public int getHeight() {
-        return height;
-    }
-
-    private int height;
+    private Point pointA;
+    private Point pointB;
+    private Point pointC;
 
     public void draw(){
-        System.out.println("The type : " + getType());
-        System.out.println("The height : "+ height);
+        System.out.println(String.format("Point A : {%d x %d}", pointA.getX(), pointA.getY()));
+        System.out.println(String.format("Point B : {%d x %d}", pointB.getX(), pointB.getY()));
+        System.out.println(String.format("Point C : {%d x %d}", pointC.getX(), pointC.getY()));
         System.out.println("Triangle has been draw");
     }
 }
