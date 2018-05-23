260b6169c6e8649490d0d05db3dd1d8486439671
Maksym Mazurkevych <maksym_mazurkevych@epam.com>
2018-05-23 11:37:45 +0300

    Injected class with 3 constructors; Made constructor by index

--- src/main/java/org/javabrains/Triangle.java
+++ src/main/java/org/javabrains/Triangle.java
@@ -10,14 +10,30 @@ public class Triangle {
 //        this.type = type;
 //    }
 
+    public Triangle(String type, int height) {
+        this.type = type;
+        this.height = height;
+    }
+
     public Triangle(String type) {
         this.type = type;
     }
 
+    public Triangle(int height) {
+        this.height = height;
+    }
+
     private String type;
+
+    public int getHeight() {
+        return height;
+    }
+
+    private int height;
 
     public void draw(){
         System.out.println("The type : " + getType());
+        System.out.println("The height : "+ height);
         System.out.println("Triangle has been draw");
     }
 }
