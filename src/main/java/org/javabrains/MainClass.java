358145c2060321e6525741b4f090efa572f206d8
Maksym Mazurkevych <maksym_mazurkevych@epam.com>
2018-05-23 09:28:13 +0300

    Introduced spring bean factory

--- src/main/java/org/javabrains/MainClass.java
+++ src/main/java/org/javabrains/MainClass.java
@@ -1,8 +1,14 @@
 package org.javabrains;
 
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 public class MainClass {
     public static void main(String[] args) {
-        Triangle triangle = new Triangle();
+//        Triangle triangle = new Triangle();
+
+        BeanFactory factory = new ClassPathXmlApplicationContext("spring-config.xml");
+        Triangle triangle = (Triangle) factory.getBean("triangle");
         triangle.draw();
     }
 }
