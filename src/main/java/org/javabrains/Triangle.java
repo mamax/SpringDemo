109246f46129acbdfd461094f415712faf51a711
Maksym Mazurkevych <maksym_mazurkevych@epam.com>
2018-05-23 13:39:32 +0300

    ST-11 Autowired beans by name

--- src/main/java/org/javabrains/Triangle.java
+++ src/main/java/org/javabrains/Triangle.java
@@ -2,16 +2,16 @@ package org.javabrains;
 
 import lombok.Data;
 
-import java.util.List;
-
 @Data
 public class Triangle {
 
-    private List<Point> points;
+    private Point pointA;
+    private Point pointB;
+    private Point pointC;
 
     public void draw(){
-        for (Point point : points){
-            System.out.println(String.format("Point : {%d x %d}", point.getX(), point.getY()));
-        }
+        System.out.println(String.format("Point A : {%d x %d}", pointA.getX(), pointA.getY()));
+        System.out.println(String.format("Point B : {%d x %d}", pointB.getX(), pointB.getY()));
+        System.out.println(String.format("Point C : {%d x %d}", pointC.getX(), pointC.getY()));
     }
 }
