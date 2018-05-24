e0277b42e7e8e799ff7c95dcb5cc51234fd6d1dd
Maksym Mazurkevych <maksym_mazurkevych@epam.com>
2018-05-24 09:23:28 +0300

    Added list of beans; Merge beans

--- src/main/java/org/javabrains/Triangle.java
+++ src/main/java/org/javabrains/Triangle.java
@@ -6,18 +6,24 @@ import org.springframework.beans.factory.BeanNameAware;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.ApplicationContextAware;
 
+import java.util.List;
+
 @Data
 public class Triangle implements ApplicationContextAware, BeanNameAware{
 
-    private Point pointA;
+    private List<Point> points;
     private Point pointB;
     private Point pointC;
     private ApplicationContext context = null;
 
     public void draw(){
-        System.out.println(String.format("Point A : {%d x %d}", pointA.getX(), pointA.getY()));
-        System.out.println(String.format("Point B : {%d x %d}", pointB.getX(), pointB.getY()));
-        System.out.println(String.format("Point C : {%d x %d}", pointC.getX(), pointC.getY()));
+//        System.out.println(String.format("Point A : {%d x %d}", pointA.getX(), pointA.getY()));
+//        System.out.println(String.format("Point B : {%d x %d}", pointB.getX(), pointB.getY()));
+//        System.out.println(String.format("Point C : {%d x %d}", pointC.getX(), pointC.getY()));
+
+        for (Point p : points){
+            System.out.println(String.format("Point : {%d x %d}", p.getX(), p.getY()));
+        }
     }
 
     public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
