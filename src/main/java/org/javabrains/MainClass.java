b84090662bf18936d181f2198b9fba04cf7827cd
Maksym Mazurkevych <maksym_mazurkevych@epam.com>
2018-05-24 10:34:51 +0300

    Added Initialization and Disposable methods to beans lifecycle

--- src/main/java/org/javabrains/MainClass.java
+++ src/main/java/org/javabrains/MainClass.java
@@ -1,14 +1,16 @@
 package org.javabrains;
 
-import org.springframework.beans.factory.BeanFactory;
+import org.springframework.context.support.AbstractApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class MainClass {
     public static void main(String[] args) {
 //        Triangle triangle = new Triangle();
 
-        BeanFactory factory = new ClassPathXmlApplicationContext("spring-config.xml");
-        Triangle triangle = (Triangle) factory.getBean("triangle-alias");
+        AbstractApplicationContext factory = new ClassPathXmlApplicationContext("spring-config.xml");
+        factory.registerShutdownHook();
+        Triangle triangle = (Triangle) factory.getBean("triangle");
         triangle.draw();
+        factory.close();
     }
 }
--- src/main/java/org/javabrains/Triangle.java
+++ src/main/java/org/javabrains/Triangle.java
@@ -1,9 +1,12 @@
 package org.javabrains;
 
 import lombok.Data;
+import org.springframework.beans.factory.BeanNameAware;
+import org.springframework.beans.factory.DisposableBean;
+import org.springframework.beans.factory.InitializingBean;
 
 @Data
-public class Triangle {
+public class  Triangle implements InitializingBean, DisposableBean, BeanNameAware {
 
     private Point pointA;
     private Point pointB;
@@ -13,5 +16,17 @@ public class Triangle {
         System.out.println(String.format("Point A : {%d x %d}", pointA.getX(), pointA.getY()));
         System.out.println(String.format("Point B : {%d x %d}", pointB.getX(), pointB.getY()));
         System.out.println(String.format("Point C : {%d x %d}", pointC.getX(), pointC.getY()));
+    }
+
+    public void setBeanName(String beanName) {
+        System.out.println("BeanName is :"+ beanName);
+    }
+
+    public void afterPropertiesSet() throws Exception {
+        System.out.println("Initialization method has been called for Triangle");
+    }
+
+    public void destroy() throws Exception {
+        System.out.println("CleanUp code before bean has been destroyed for Triangle");
     }
 }
